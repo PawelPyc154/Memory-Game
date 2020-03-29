5431a73c533c4e41ffdef61a1e07914c79abbf94
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-03-29 18:13:21 +0200

    Updates

---
+++ precache-manifest.e788364d24d4f7f2b833e3473488c6ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6022d7994f777f170b4db75c16faef2b",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "8a6aa4a68640d13ac5bd",
+    "url": "/Memory-Game/static/css/main.a7d44374.chunk.css"
+  },
+  {
+    "revision": "1fd6883653cc1a59f1bb",
+    "url": "/Memory-Game/static/js/2.2da161b9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Memory-Game/static/js/2.2da161b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6aa4a68640d13ac5bd",
+    "url": "/Memory-Game/static/js/main.f7293d44.chunk.js"
+  },
+  {
+    "revision": "337133ff866a0ca155ed",
+    "url": "/Memory-Game/static/js/runtime-main.28b5bdaf.js"
+  }
+]);
